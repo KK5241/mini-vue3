a2228a9753c2221ee58b62b86419a250cf849e69
xiaoka <11076632@qq.com>
2024-10-07 09:13:58 +0800

    feat: 完成effect返回runer、stop、scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,8 +1,16 @@
 class ReactiveEffect {
-  constructor(public fn) {}
+  arr = []
+  constructor(public fn, public scheduler?) {}
   run() {
-    activeEffecr = this
-    this.fn();
+    activeEffect = this; //为什么传整个实例，而不是只传一个数组，通过后面需要scheduler可知，传实例的话更容易拿到scheduler
+    const res = this.fn();
+    return res;
+  }
+  stop(){
+    console.log(this.arr);
+    this.arr.forEach((dep:any)=>{
+      dep.delete(this)
+    })
   }
 }
 
@@ -13,23 +21,42 @@ export const track = function (target, key) {
   if (!deps) depsMap.set(target, (deps = new Map()));
   let dep = deps.get(key);
   if (!dep) deps.set(key, (dep = new Set()));
-  dep.add(activeEffecr)
+  if(!activeEffect) return
+  dep.add(activeEffect);
+  activeEffect.arr.push(dep)
 };
 //派发更新函数
 export const trigger = function (target, key) {
-    const deps = depsMap.get(target)
-    if(!deps) return 
-    const dep = deps.get(key)
-    if(!dep) return 
-    dep.forEach(element => {
-        element.run()
-    });
+  const deps = depsMap.get(target);
+  if (!deps) return;
+  const dep = deps.get(key);
+  if (!dep) return;
+  dep.forEach((element) => {
+    if(element.scheduler){
+        element.scheduler()
+    }else{
+        element.run();
+    }
+  });
 };
 
 //effect辅助函数
-let activeEffecr
-export const effect = function (fn) {
-  const _effect = new ReactiveEffect(fn);
+let activeEffect;
+export const effect = function (fn, options: any = {}) {
+  const scheduler = options.scheduler;
+  const _effect = new ReactiveEffect(fn,scheduler);
 
   _effect.run();
+  
+  const runner:any = _effect.run.bind(_effect);
+  //返回的函数添加当前实例属性，方便根据返回的函数找到对应的实例 方便stop操作
+  runner._effect = _effect
+   
+  // activeEffect = undefined
+
+  return runner
 };
+
+export const stop = function(runner){
+  runner._effect.stop()
+}
--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -1,21 +1,77 @@
-import { effect } from "../effect"
-import { reactive } from "../reactive"
+import { effect,stop } from "../effect";
+import { reactive } from "../reactive";
 
-describe('effect',()=>{
-    it('happy path',()=>{
-        const obj = reactive({
-            foo:1
-        })
+describe("effect", () => {
+  it("happy path", () => {
+    const obj = reactive({
+      foo: 1,
+    });
 
-        let acticveValue
-        effect(()=>{
-            acticveValue = obj.foo + 1
-        })
+    let acticveValue;
+    effect(() => {
+      acticveValue = obj.foo + 1;
+    });
 
-        expect(acticveValue).toBe(2)
+    expect(acticveValue).toBe(2);
 
-        obj.foo ++
-        
-        expect(acticveValue).toBe(3)
+    obj.foo++;
+
+    expect(acticveValue).toBe(3);
+  });
+  it("should return runner", () => {
+    let x = 1;
+    const fn = effect(() => {
+      x++;
+      return "foo";
+    });
+
+    expect(x).toBe(2);
+    const m = fn();
+    expect(x).toBe(3);
+    expect(m).toBe("foo");
+  });
+
+  it("scheduler", () => {
+    let dummy;
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo + 1;
+        return "foo";
+      },
+      {
+        scheduler,
+      }
+    );
+    expect(dummy).toBe(2);
+    expect(scheduler).not.toHaveBeenCalled();
+    obj.foo++;
+    expect(dummy).not.toBe(3);
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    const res = run();
+    expect(dummy).toBe(3);
+    expect(res).toBe("foo");
+  });
+
+  it('stop',()=>{
+    let dummy
+    let obj = reactive({foo:1})
+    const runner = effect(()=>{
+      dummy = obj.foo + 1
     })
-})
+    expect(dummy).toBe(2)
+    obj.foo = 2
+    expect(dummy).toBe(3)
+    stop(runner)
+    // 目前如果这里用 obj.foo ++ 的还是会存在问题，他会错误的把之前的依赖函数收集起来
+    obj.foo = 3
+    expect(dummy).toBe(3)
+    runner()
+    expect(dummy).toBe(4)
+  })
+});
