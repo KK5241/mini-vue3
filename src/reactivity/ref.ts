4676c7862ce47a10eda455371298193daf88f3ab
xiaoka <11076632@qq.com>
2024-10-12 15:35:06 +0800

    fix: 代码格式优化

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -5,12 +5,12 @@ import { reactive } from "./reactive";
 export class RefImpl {
   private _value;
   private _dep;
-  private _rawValue
-  private _v_isRef = true
+  private _rawValue;
+  private _v_isRef = true;
   constructor(value) {
-    this._rawValue = value
+    this._rawValue = value;
     // ref 本身只能对value属性进行响应式的拦截操作 所以这里将value中的对象转化为reactive 从而实现深度响应式拦截
-    this._value = conver(value)
+    this._value = conver(value);
     this._dep = new Set();
   }
 
@@ -22,41 +22,41 @@ export class RefImpl {
   }
 
   set value(newValue) {
-    if(hasChange(newValue,this._rawValue)) return 
-    this._rawValue = newValue
-    this._value = conver(newValue)
-    triggerEffects(this._dep)
+    if (hasChange(newValue, this._rawValue)) return;
+    this._rawValue = newValue;
+    this._value = conver(newValue);
+    triggerEffects(this._dep);
   }
 }
 
-function conver(value){
-    return isObject(value) ? reactive(value) : value;
+function conver(value) {
+  return isObject(value) ? reactive(value) : value;
 }
 
 export function ref(value) {
   return new RefImpl(value);
 }
 
-export function isRef(value){
-  return !!value._v_isRef
+export function isRef(value) {
+  return !!value._v_isRef;
 }
 
-export function unref(value){
-  return isRef(value) ? value.value : value
+export function unref(value) {
+  return isRef(value) ? value.value : value;
 }
 
-export function proxyRefs(obj){
-  return new Proxy(obj,{
-    get(target,key){
-      return unref(Reflect.get(target,key))
+export function proxyRefs(obj) {
+  return new Proxy(obj, {
+    get(target, key) {
+      return unref(Reflect.get(target, key));
     },
-    set(target,key,value){
-      if(isRef(target[key]) && !isRef(value)){
-        target[key].value = value
-      }else{
-        Reflect.set(target,key,value)
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        target[key].value = value;
+      } else {
+        Reflect.set(target, key, value);
       }
-      return true
-    }
-  })
+      return true;
+    },
+  });
 }
