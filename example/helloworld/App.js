639ab1691077c43b93eeab92091c1f9fd4a46bee
xiaoka <11076632@qq.com>
2024-10-15 09:50:52 +0800

    feat: 利用小步骤思想实现注册事件

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -8,7 +8,13 @@ export const App = {
         window.self = this
         //这里是给 h 函数传递参数，参数中的this使用的时render的this，render中的this会在调用的时候绑定
         return h('div', {
-            id: 'root'
+            id: 'root',
+            onClick(){
+                console.log('click');
+            },
+            onMousedown(){
+                console.log('mouseDown');
+            }
         }, [h('h1',{},'123')])
     },
 
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -7,7 +7,7 @@ export function render(vNode, container) {
 }
 function patch(vNode, container) {
   //存在两种可能 一种是组件  一种是element元素
-  const {shapeFlag} = vNode
+  const { shapeFlag } = vNode;
   if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vNode, container);
   } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
@@ -24,7 +24,7 @@ function processElement(vNode, container) {
 function mountElement(vNode, container) {
   // 递归遍历的时候为每个虚拟DOM 绑定当前的元素
   const el = (vNode.el = document.createElement(vNode.type));
-  const { props, children,shapeFlag } = vNode;
+  const { props, children, shapeFlag } = vNode;
 
   //为真实DOM el设置属性
   setAttributes(el, props);
@@ -53,9 +53,9 @@ function setupRenderEffect(instance, vNode, container) {
   const subTree = instance.render.call(proxy);
   //递归调用patch进一步操作组件内的元素
   patch(subTree, container);
-  //subTree 是组件第一次拆箱的结果 也就是根元素的虚拟DOM  
-  //这里是将组件的 el 绑定为根元素的 el 
-  vNode.el = subTree.el
+  //subTree 是组件第一次拆箱的结果 也就是根元素的虚拟DOM
+  //这里是将组件的 el 绑定为根元素的 el
+  vNode.el = subTree.el;
 }
 
 //处理elementVnode中child为数组的情况
@@ -69,7 +69,19 @@ function setAttributes(el: any, props: any) {
   for (const key in props) {
     if (Object.prototype.hasOwnProperty.call(props, key)) {
       const val = props[key];
-      el.setAttribute(key, val);
+      // 当props是注册事件时通过这种方式注册，利用小步骤思想，具体 -> 抽象
+      // on + 事件名（这里的首字母要大写）
+      //判断的时候可以用正则，处理的时候切割+转化小写 
+      // if (key === "onClick") {
+      //   el.addEventListener("click", val);
+      //}
+      const isMatch = /^on[A-Z]/.test(key)
+      if(isMatch){
+        const transformEvent = key.slice(2).toLocaleLowerCase()
+        el.addEventListener(transformEvent,val)
+      }else{
+        el.setAttribute(key, val);
+      }
     }
   }
 }
